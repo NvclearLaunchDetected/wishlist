f3f1bcd794c77293d7bfbaa930a17603617fab5d
sungwpark <sungwpark@ebay.com>
2012-09-26 17:57:57 +0900

    issue#3 WIP

--- src/extension/scripts/background.js
+++ src/extension/scripts/background.js
@@ -8,11 +8,16 @@ var url_parser = new URLParser();
 
 //browser action
 chrome.browserAction.onClicked.addListener(function(tab){
-  if(!google.getAccessToken()){
+  //기본 browser action 버튼은 무기능?
+  //혹은 사용 안내 페이지?
+  //그저 인증 수행할 뿐...
+
+  /*if(!google.getAccessToken()){
     google.authorize(function(){
       //api 완성되면 여기서 등록 call 날려야지~~    
     })
-  }
+  }*/
+  
 })
 
 //tabs handler
@@ -20,9 +25,17 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
   //현재 tab의 loading된 url이 변경 되면 패턴 조회
   if(!url_parser.isProduct(changeInfo.url)) {
     chrome.browserAction.setBadgeText({text:'', tabId: tabId});
+    chrome.browserAction.setPopup({
+      tabId: tab.tabId,
+      popup: ''
+    });
     return;
   }
 
   //선택된 tabId로 한정됨. 다른 tab은 자동으로 badge 변경
   chrome.browserAction.setBadgeText({text:'+', tabId: tabId});
+  chrome.browserAction.setPopup({
+    tabId: tab.tabId,
+    popup: 'AddToWishList.html'
+  });
 })
