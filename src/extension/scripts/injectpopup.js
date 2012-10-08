b18758c37c86cf61f45a59705b5f9d0e5d55b9df
sungwpark <sungwpark@ebay.com>
2012-10-08 13:16:44 +0900

    ISSUE#3 WIP. imagelist prev, next action done

--- src/extension/scripts/injectpopup.js
+++ src/extension/scripts/injectpopup.js
@@ -10,8 +10,19 @@ function inject(){
 	chrome.extension.sendMessage('getPopupHtml', function(res){
 		popup.append(res.html);
 		alreadyInjected = true;
-		//add event handlers
 
+		//scrapping data
+		
+		var scraper = new PageScraper();
+		var selectedImageIndex = 0;
+		var imageList = scraper.getGenericImageData();
+
+		$('#wishlist_popup_title').val(scraper.getTitle());
+		$('#wishlist_popup_price').val(scraper.getPrice());
+		$('#wishlist_popup_imagelist_img').attr('src', imageList[selectedImageIndex].src);
+		$('#wishlist_popup_imagelist_count').text(selectedImageIndex+1 + ' / ' + imageList.length);
+
+		//add event handlers
 		$('.wishlist_popup .close').click(function(){
 			setTimeout(function(){
 				$('.wishlist_popup').remove();
@@ -19,11 +30,17 @@ function inject(){
 			},0)
 		})
 
-		//scrapping data
-		var scraper = new PageScraper();
-		$('#wishlist_popup_title').val(scraper.getTitle());
-		$('#wishlist_popup_price').val(scraper.getPrice());
-		
+		$('#wishlist_popup_imagelist_prev').click(function(){
+			if(selectedImageIndex>0) selectedImageIndex--;
+			$('#wishlist_popup_imagelist_img').attr('src', imageList[selectedImageIndex].src);
+			$('#wishlist_popup_imagelist_count').text(selectedImageIndex+1 + ' / ' + imageList.length);
+		})
+
+		$('#wishlist_popup_imagelist_next').click(function(){
+			if(selectedImageIndex<imageList.length) selectedImageIndex++;
+			$('#wishlist_popup_imagelist_img').attr('src', imageList[selectedImageIndex].src);
+			$('#wishlist_popup_imagelist_count').text(selectedImageIndex+1 + ' / ' + imageList.length);
+		})
 	});
 }
 
