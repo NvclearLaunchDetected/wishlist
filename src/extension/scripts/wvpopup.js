514ada4c78ee6245d2f34ec10feba1251d8ea304
Jinyong Lee <jinylee@ebay.com>
2012-10-09 14:43:46 +0900

    update getting backgroundpage

--- src/extension/scripts/wvpopup.js
+++ src/extension/scripts/wvpopup.js
@@ -1,28 +1,42 @@
-var wvpopup = {
-	injected: false,
-	show: function(tabId) {
-		if (wvpopup.injected) return;
-
-		var popup = $(document.createElement('table')).attr("id", "wvpopup_k2tw4YzP50x9BemQp").attr("tid", tabId);
-		popup.attr('width','518px').attr('border',0).attr('cellpadding',0).attr('cellspacing',0).addClass('wishlist_popup');
-		$(document.getElementsByTagName('body')).append(popup);
-
-		chrome.extension.sendMessage({msg: 'getListPopupHtml'}, function(res){
-			popup.append(res.html);
-
-			$('.wvpopup_close').click(function(){
-				wvpopup.hide(tabId);
-			});
-
-			wvpopup.injected = true;
-		});
-	},
-	hide: function(tabId) {
-		if (!wvpopup.injected) return;
-
-		setTimeout(function(){
-			$('#wvpopup_k2tw4YzP50x9BemQp').remove();
-			wvpopup.injected = false;
-		}, 0);
+var _px = {
+	google: undefined,
+	load: function(cb) {
+		$.ajax({
+			type: 'GET',
+			url: 'http://wishapi-auth.cloudfoundry.com/wishlist',
+			headers: {
+				'GX-AUTH': 'ga=' + _px.google.userinfo.email + '&token=' + _px.google.getAccessToken()
+			}
+		})
+		.done(function(data) {
+			console.log(JSON.stringify(data));
+			cb(data);
+		})
+		.error(function(error) {
+			cb({err:{msg: 'unknown error!'}});
+		})
 	}
 };
+
+var _uv = {
+	render: function(d) {
+		var h = "";
+		for (var i = 0; i < d.length; i++) {
+			h += "<tr><td></td><td></td><td></td><td></td></tr>";
+		}
+
+		$("#wvlist").append(h);
+	}
+}
+
+$(document).ready(function() {
+	_px.google = chrome.extension.getBackgroundPage().google;
+
+	console.log(JSON.stringify(_px.google));
+
+	_px.load(function(data) {
+		if (!data.err) {
+			_uv.render(data);
+		}
+	});
+});
