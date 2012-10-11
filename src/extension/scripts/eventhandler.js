2dcbb49625f175d3ba936eaba3e267993b61cee3
sungwpark <sungwpark@ebay.com>
2012-10-11 13:42:23 +0900

    refactoring eventhandler

--- src/extension/scripts/eventhandler.js
+++ src/extension/scripts/eventhandler.js
@@ -1,47 +1,31 @@
 var url_parser = new URLParser();
 
+function setDefault(tabId){
+	chrome.browserAction.setBadgeText({text:'', tabId: tabId});
+	//set wish list as a default popup
+	chrome.browserAction.setPopup({
+		tabId: tabId,
+		popup: 'wvpopup.html'
+	});
+}
+
+function setAddMode(tabId){
+	chrome.browserAction.setBadgeText({text:'+', tabId: tabId});
+	chrome.browserAction.setPopup({
+		tabId: tabId,
+		popup: 'awpopup.html'
+	});
+}
+
 //tabs handler
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-	//„ìž¬ tabloadingurlë³€ê²˜ë©´ ¨í„´ ì¡°íšŒ
-	console.log('onUpdated')
 	if(!url_parser.isValid(tab.url)) {
-		chrome.browserAction.setBadgeText({text:'', tabId: tabId});
-		//set wish list as a default popup
-		chrome.browserAction.setPopup({
-			tabId: tab.tabId,
-			popup: 'wvpopup.html'
-		});
+		setDefault(tabId);
 		return;
 	}
 
-	// íƒtabIdë¡œì • ¤ë¥¸ tab€ ë™¼ë¡œ badge ë³€ê²
-	chrome.browserAction.setBadgeText({text:'+', tabId: tabId});
-	 chrome.browserAction.setPopup({
-		 tabId: tab.tabId,
-		 popup: 'awpopup.html'
-	 });
+	setAddMode(tabId);
 })
-
-chrome.tabs.onActivated.addListener(function(activeInfo){
-	console.log('onActivated')
-  chrome.browserAction.getBadgeText({
-    tabId: activeInfo.tabId
-  }, function(badge){
-    if(badge == '+'){
-      chrome.browserAction.setPopup({
-       tabId: activeInfo.tabId,
-       popup: 'awpopup.html'
-     });
-    }
-    else{
-      chrome.browserAction.setPopup({
-        tabId: activeInfo.tabId,
-        popup: 'wvpopup.html'
-      });
-    }
-  })
-})
-
 
 chrome.extension.onMessage.addListener(function(info, sender, cb){
 	if ('getProductInfo' == info.msg){
