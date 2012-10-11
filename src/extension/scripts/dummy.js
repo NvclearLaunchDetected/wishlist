55d1257eb3628a9ef31a77097471270445c9071b
Lee, Tai Hee <tailee@ebay.com>
2012-10-11 12:57:28 +0900

    remove extentions for devide project on branch

--- src/extension/scripts/dummy.js
+++
@@ -1,9 +0,0 @@
-//api 완성되면 곧 사라질 script 파일...
-function printUserInfo(userinfo){
-	$('#userInfo').html(userinfo);
-}
-
-var background = chrome.extension.getBackgroundPage();
-printUserInfo(background.google.getAccessToken());
-
-
